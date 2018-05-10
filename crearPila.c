ef2b1e7f73b901cfe364b64f3d5222a518359e98
avilesdiana <avilesdiana@outlook.com>
2018-05-09 20:13:42 -0500

    Archivos

--- crearPila.c
+++
@@ -1,9 +0,0 @@
-#include "funciones.h"
-
-pilaNodoE *creaPila (int nodoss) {
-    pilaNodoE *auxiliarPila = calloc(1, sizeof (pilaNodoE));
-    auxiliarPila->datosAlmacenados = calloc(nodoss + 1, sizeof (int));
-    auxiliarPila->auxNodo = calloc(nodoss + 1, sizeof (int));
-    auxiliarPila->indice = calloc(nodoss, sizeof (int));
-    return auxiliarPila;
-}
